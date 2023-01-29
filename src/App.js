abb002d9412f6ab009c8d2a71807283c82c92092
reallithuanianmafia <reallithuanianmafia@outlook.com>
2023-01-29 16:35:48 +0200

    Counter App

--- src/App.js
+++ src/App.js
@@ -1,10 +1,32 @@
+import { useState } from 'react';
 import './App.css';
 
 function App() {
 
+  const [ count, setCount ] = useState(0);
+
+  const popUp = () => {
+    setCount((prevState) => prevState - 1);
+  }
+
+  const pushUp = (prevState) => {
+    setCount((prevState) => prevState + 1);
+  }
+
   return (
     <div className='main container-fluid'>
-      Lorem
+      <div className='row'>
+        <div className='col-md-12'>
+        <div className="card text-bg-dark mb-3">
+          <div className="card-header">Counter App</div>
+          <div className="card-body">
+            <h5 className="card-title">Count: {count} </h5>
+            <p><button className='btn btn-primary' onClick = { () => pushUp() }>Push</button></p>
+            <p><button className='btn btn-danger' onClick = { () => popUp() }>Pop</button></p>
+          </div>
+        </div>
+        </div>
+      </div>
     </div>
   );
 }
